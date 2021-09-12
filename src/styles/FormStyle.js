269412eb63bf32875a2907777c7ceb6cba03b6f5
DavidJC79 <davidj79c@gmail.com>
2021-09-12 05:34:08 -0500

    Se dio orden en las propiedades de los estilos para mejor lectura

--- src/styles/FormStyle.js
+++ src/styles/FormStyle.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
 export const FormStyle = styled.div`
-    min-width: 7.5em;
-    min-height: 10em;
-    width: 27.5em;
-    height: 35em;
     display: flex;
-    flex-direction:column;
-    margin-top: 2rem;
     box-shadow: -1px 26px 81px -30px rgba(143,20,65,0.88);
     -webkit-box-shadow: -1px 26px 81px -30px rgba(143,20,65,0.88);
     -moz-box-shadow: -1px 26px 81px -30px rgba(143,20,65,0.88);
+    flex-direction:column;
+    height: 35em;
+    margin-top: 2rem;
+    min-width: 7.5em;
+    min-height: 10em;
+    width: 27.5em;
 `;
